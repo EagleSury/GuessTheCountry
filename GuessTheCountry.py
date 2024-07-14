31faf034b26f97cd0ed5dba055954c149677b61a
EagleSury <155902090+EagleSury@users.noreply.github.com>
2024-07-14 20:41:11 +0300

    Create GuessTheCountry.py

---
+++ GuessTheCountry.py
@@ -0,0 +1,11 @@
+# 1 create a function that does the webscraping
+
+# 2 function that chooses a random flag and 4 random countries
+
+# 3 open html rendered with 2
+
+# 4 function that gets the input
+
+# 5 function that checks if the input (from 4) is right or wrong and calls 6 with the appropiate message
+
+# 6 opens html rendered with 5
