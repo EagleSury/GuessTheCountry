3f64b11d7e816168336789a433fc5d531e30a4b3
EagleSury <155902090+EagleSury@users.noreply.github.com>
2024-07-25 19:55:39 +0000

    did basic html , added basic webscraping - didn't test either of them

--- WebScraping.py
+++ WebScraping.py
@@ -1,72 +1,35 @@
-# Import Module
-from bs4 import *
+'''import pandas as pd
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+tables = pd.read_html('https://en.wikipedia.org/wiki/Gallery_of_sovereign_state_flags')
+print(len(tables))
+
+print(tables[1]) '''
+'''
 import requests
- 
-# Given URL
-url = "https://en.wikipedia.org/wiki/Beautiful_Soup_(HTML_parser)"
- 
-# Fetch URL Content
-r = requests.get(url)
- 
-# Get body content
-soup = BeautifulSoup(r.text,'html.parser').select('body')[0]
- 
-# Initialize variable
-paragraphs = []
-images = []
-link = []
-heading = []
-remaining_content = []
- 
-# Iterate through all tags
-for tag in soup.find_all():
-     
-    # Check each tag name
-    # For Paragraph use p tag
-    if tag.name=="p":
-       
-        # use text for fetch the content inside p tag
-        paragraphs.append(tag.text)
-         
-    # For Image use img tag
-    elif tag.name=="img":
-       
-        # Add url and Image source URL
-        images.append(url+tag['src'])
-         
-    # For Anchor use a tag
-    elif tag.name=="a":
-       
-        # convert into string and then check href 
-        # available in tag or not
-        if "href" in str(tag):
-           
-          # In href, there might be possible url is not there
-          # if url is not there
-            if "https://en.wikipedia.org/w/" not in str(tag['href']):
-                link.append(url+tag['href'])
-            else:
-                link.append(tag['href'])
-                 
-    # Similarly check for heading 
-    # Six types of heading are there (H1, H2, H3, H4, H5, H6)
-    # check each tag and fetch text
-    elif "h" in tag.name:
-        if "h1"==tag.name:
-            heading.append(tag.text)
-        elif "h2"==tag.name:
-            heading.append(tag.text)
-        elif "h3"==tag.name:
-            heading.append(tag.text)
-        elif "h4"==tag.name:
-            heading.append(tag.text)
-        elif "h5"==tag.name:
-            heading.append(tag.text)
-        else:
-            heading.append(tag.text)
-             
-    # Remain content will store here
-    else:
-        remaining_content.append(tag.text)
-         
-print(paragraphs, images, link, heading, remaining_content)
+page = requests.get('https://en.wikipedia.org/wiki/Gallery_of_sovereign_state_flags')
+
+print(page.content)
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(page.content, 'html.parser')
+print(soup.prettify())
+list(soup.children)
+print(soup.find_all('a'))'''
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/Gallery_of_sovereign_state_flags"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+flags_dict = {}
+for img in soup.find_all('img'):
+    if 'Flag' in img['src']:
+        country_name = img['alt']
+        flag_url = 'https:' + img['src']
+        flags_dict[country_name] = flag_url
+
+print(flags_dict)
--- test.py
+++ test.py
@@ -1,1 +1,3 @@
+def test_country_in_flags(country, my_flags):
+    return country in my_flags.get(country, "")
 
